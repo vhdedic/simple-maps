e44445fb1dd4f8fcd1d800b59566fab48fd967a2
vhdedic <vdedic@gmail.com>
2022-11-02 16:45:16 +0100

    Create templates/name/create.html.twig

--- src/Controller/NameController.php
+++ src/Controller/NameController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Name;
+use App\Form\NameFormType;
 use App\Repository\NameRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class NameController extends AbstractController
 {
-    public function __construct(NameRepository $nameRepository)
+    public function __construct(EntityManagerInterface $em, NameRepository $nameRepository)
     {
+        $this->em = $em;
         $this->nameRepository = $nameRepository;
     }
     
@@ -21,6 +26,30 @@ class NameController extends AbstractController
         
         return $this->render('name/index.html.twig', [
             'names' => $names
+        ]);
+    }
+
+    #[Route('/name/create', name: 'app_name_create')]
+    public function create(Request $request): Response
+    {
+        $name = new Name;
+
+        $form = $this->createForm(NameFormType::class, $name);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()) {
+            $name->setName($form->get('name')->getData());
+            $name->setLongitude($form->get('longitude')->getData());
+            $name->setLatitude($form->get('latitude')->getData());
+
+            $this->em->persist($name);
+            $this->em->flush();
+
+            return $this->redirectToRoute('app_name_index');
+        }
+
+        return $this->render('name/create.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
