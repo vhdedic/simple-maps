8e5116ff364d639dc2e015f8a9a88704c3fd3f39
vhdedic <vdedic@gmail.com>
2022-11-02 14:44:34 +0100

    Create src/Form/NameFormType.php

---
+++ src/Form/NameFormType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Name;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NameFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('longitude')
+            ->add('latitude')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Name::class,
+        ]);
+    }
+}
