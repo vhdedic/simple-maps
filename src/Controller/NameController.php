17b1493bfc162cc87a7bd7afd444f1081526487f
vhdedic <vdedic@gmail.com>
2022-11-02 10:53:25 +0100

    Edit templates/name/index.html.twig

--- src/Controller/NameController.php
+++ src/Controller/NameController.php
@@ -2,15 +2,25 @@
 
 namespace App\Controller;
 
+use App\Repository\NameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class NameController extends AbstractController
 {
+    public function __construct(NameRepository $nameRepository)
+    {
+        $this->nameRepository = $nameRepository;
+    }
+    
     #[Route('/names', name: 'app_name_index')]
     public function index(): Response
     {
-        return $this->render('name/index.html.twig');
+        $names = $this->nameRepository->findAll();
+        
+        return $this->render('name/index.html.twig', [
+            'names' => $names
+        ]);
     }
 }
