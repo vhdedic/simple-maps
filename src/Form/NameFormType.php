63fc10e2539ce453d3afcd9302421a244ea570ee
vhdedic <vdedic@gmail.com>
2022-11-04 14:22:45 +0100

    Edit src/Form/NameFormType.php

--- src/Form/NameFormType.php
+++ src/Form/NameFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Name;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +14,12 @@ class NameFormType extends AbstractType
     {
         $builder
             ->add('name')
-            ->add('longitude')
-            ->add('latitude')
+            ->add('longitude', NumberType::class, [
+                'scale' => 5,
+            ])
+            ->add('latitude', NumberType::class, [
+                'scale' => 5,
+            ])
         ;
     }
 
